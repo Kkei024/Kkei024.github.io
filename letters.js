964053f6f3816c01766021408341794dbbafece5
Kei <very0riginal3mail@gmail.com>
2026-07-20 03:18:16 +0800

    Add files via upload
    i'm trying

---
+++ letters.js
@@ -0,0 +1,80 @@
+console.log("letters test");
+let blanks = document.getElementById('lettercont');
+
+let words = [
+    "STATE",
+    "GOVERNMENT",
+    "SOVEREIGNTY",
+    "ACCOUNTABILITY",
+    "BRANCHES",
+    "BOUNDARIES",
+    "POWER",
+    "TERRITORY",
+    "RECOGNITION",
+    ""
+];
+let i = 0;
+
+function question() {
+    let curren = words[i];
+
+    localStorage.setItem("wordpicker", curren);
+
+    console.log("curren says "+ curren);
+
+    let kawaii = curren.length;
+
+    blanks.innerHTML = `
+        <input type="text" class="inp" id="ans" maxlength="${kawaii}" autofocus>
+    `;
+
+    
+    function disp() {
+        for (let x = 0 ; x < kawaii ; x++) {
+            
+            blanks.innerHTML += `
+            <div class="blank">
+            <h2 id="text${x}"></h2>
+            </div>
+            `;
+        }
+    } disp()
+
+    
+
+    document.addEventListener("keydown", (held) => {
+        if (held.repeat) {
+            held.preventDefault();
+        }
+
+        let answer = document.getElementById('ans').value;
+        let y = answer.length - 1;
+
+        if ((y < kawaii) && (held.key != "Backspace")) {
+            document.getElementById(`text${y}`).innerText = `${answer[y]}`;
+        }
+    })
+    document.addEventListener("keyup", function(pressed){
+        let answer = document.getElementById('ans').value;
+        let y = answer.length - 1;
+
+
+        if ((pressed.key == "Backspace")) {
+            y += 1;
+            document.getElementById(`text${y}`).innerText = ``;
+        }
+
+        else if (y < kawaii) {
+            document.getElementById(`text${y}`).innerText = `${answer[y]}`;
+        }
+
+        
+    });
+} 
+
+question();
+
+function nextQ() {
+    i = Number(localStorage.getItem('numbers'));
+    question();
+}
